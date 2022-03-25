8025bb3fcb69ca2415b3428ce600e919e2f19c80
SQ6NTI <miskrzyn@gmail.com>
2022-03-25 22:28:43 +0100

    mapillary_tools update to 0.8.2

--- geotag.sh
+++ geotag.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Prepares 360 files for upload to Mapillary
 
-[ $# -eq 0 ] && { echo "Usage: $0 <GPXFile> <ImageDir> <MapillaryUsername>"; exit 1; }
+[ $# -eq 0 ] && { echo "Usage: $0 <GPXFile> <ImageDir>"; exit 1; }
 
-./mapillary_tools process --advanced --import_path "$2" --user_name "$3" --geotag_source "gpx" --geotag_source_path "$1" --overwrite_EXIF_gps_tag --rerun --verbose
+./mapillary_tools process "$2" --geotag_source "gpx" --geotag_source_path "$1" --overwrite_EXIF_gps_tag
